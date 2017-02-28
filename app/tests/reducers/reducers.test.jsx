ea34f55994eb8aea6ab6112104e460741534b1db
FatCat32 <antoine.mctaggart@gmail.com>
2017-02-28 08:11:04 -0500

    add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,6 @@
 var expect = require('expect');
 var df = require('deep-freeze-strict');
+var moment = require('moment');
 
 var reducers = require('reducers');
 
@@ -26,6 +27,76 @@ describe ('Reducers', () => {
       var res = reducers.showCompletedReducer(df(false), df(action));
 
       expect(res).toEqual(true);
+    });
+  });
+
+  describe ('-- todosReducer', () => {
+    it('should add new todo ', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Histoire'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual('Histoire');
+    });
+
+    it('should toggleTodo first todo', () => {
+      var todos = [{
+        id: 1,
+        text: 'wash the dog',
+        completed: false,
+        createdAt: 0,
+        completedAt: undefined
+      },
+      {
+        id: 2,
+        text: 'wash the cat',
+        completed: true,
+        createdAt: 111,
+        completedAt: 125
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toEqual(moment().unix());
+      expect(res[1].completed).toEqual(true);
+      expect(res[1].completedAt).toEqual(125);
+    });
+
+    it('should toggleTodo second todo', () => {
+      var todos = [{
+        id: 1,
+        text: 'wash the dog',
+        completed: false,
+        createdAt: 0,
+        completedAt: undefined
+      },
+      {
+        id: 2,
+        text: 'wash the cat',
+        completed: true,
+        createdAt: 111,
+        completedAt: 125
+      }];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 2
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+      expect(res[1].completed).toEqual(false);
+      expect(res[1].completedAt).toEqual(undefined);
     });
   });
 });
