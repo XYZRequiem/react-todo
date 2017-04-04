e99e81ca5b6079d16f863f02b80398517e39d30d
FatCat32 <antoine.mctaggart@gmail.com>
2017-04-04 09:01:49 -0400

    secured apikeys and env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,10 +3,10 @@ import firebase from 'firebase';
 // Initialize Firebase
  try {
    var config = {
-     apiKey: "AIzaSyCqE5lW7K9ZynQK6s_xIwglAMhQ6sca_zo",
-     authDomain: "histoire-todo-app.firebaseapp.com",
-     databaseURL: "https://histoire-todo-app.firebaseio.com",
-     storageBucket: "histoire-todo-app.appspot.com",
+     apiKey: process.env.API_KEY,
+     authDomain: process.env.AUTH_DOMAIN,
+     databaseURL: process.env.DATABASE_URL,
+     storageBucket: process.env.STORAGE_BUCKET,
      messagingSenderId: "626841654372"
    };
 
